20d1877e926b8c1c025b331de4ece82143b1b418
Ava Weiner <avaw@umich.edu>
2017-10-30 15:30:55 -0400

    oct30 lecture

---
+++ Week8.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('mboxdb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = open('mbox.txt')
+for line in fname:
+    if not line.startswith('From '): continue
+    parts = line.split()
+    email = parts[1].split("@")[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (email,))
+    conn.commit()
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 1'
+
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
